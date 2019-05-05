2b166f22f20d9bdf940bc648b3321b46e38634d3
hussein18149 <husseinkatana17@gmail.com>
2019-05-05 05:10:22 +0300

    added hover effects on the images and styled them further

---
+++ script.js
@@ -0,0 +1,9 @@
+$( ".img1" ).click(function() {
+    $( ".p1" ).toggle();
+  })
+    $( ".img2" ).click(function() {
+    $( ".p2" ).toggle();
+    })
+    $( ".img3" ).click(function() {
+    $( ".p3" ).toggle();
+  });
